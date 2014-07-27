84a504bf54059dfbfb28d28bf582db15337f3ef3
VitaliyOliynyk <xaoc00@gmail.com>
2014-07-27 02:19:36 +0200

    initial commit

---
+++ thrift-calculator.thrift
@@ -0,0 +1,28 @@
+namespace java thrift.calculator
+namespace php thrift.calculator
+namespace csharp thrift.calculator
+
+enum Operation {
+  ADD = 1,
+  SUBTRACT = 2,
+  MULTIPLY = 3,
+  DIVIDE = 4
+}
+
+struct Work {
+    1: i32 arg1,
+    2: i32 arg2,
+    3: Operation operacja
+}
+
+exception InvalidOperation {
+  1: string why
+}
+
+service CalculatorService {
+    i32 calculate(1:Work work) throws (1:InvalidOperation ouch)
+}
+
+
+
+
